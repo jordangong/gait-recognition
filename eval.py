36cf502afe9b93efe31c244030270b0a62e644b8
Jordan Gong <jordan.gong@protonmail.com>
2021-01-12 11:13:39 +0800

    Print result after evaluation

--- eval.py
+++ eval.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from config import config
 from models import Model
 from utils.dataset import ClipConditions
@@ -14,3 +16,15 @@ dataset_selectors = {
 
 accuracy = model.predict_all(config['model']['total_iter'], config['dataset'],
                              dataset_selectors, config['dataloader'])
+rank = 5
+np.set_printoptions(formatter={'float': '{:5.2f}'.format})
+for n in range(rank):
+    print(f'===Rank-{n + 1} Accuracy===')
+    for (condition, accuracy_c) in accuracy.items():
+        acc_excl_identical_view = accuracy_c[:, :, n].fill_diagonal_(0)
+        num_gallery_views = (acc_excl_identical_view != 0).sum()
+        acc_each_angle = acc_excl_identical_view.sum(0) / num_gallery_views
+        print('{0}: {1} mean: {2:5.2f}'.format(
+            condition, acc_each_angle.cpu().numpy() * 100,
+            acc_each_angle.mean() * 100)
+        )
