9bd6908ed77410e2efa439d26ebd1f9b60519f53
Jordan Gong <jordan.gong@protonmail.com>
2021-01-12 11:43:11 +0800

    Some type hint fixes

--- models/model.py
+++ models/model.py
@@ -315,7 +315,7 @@ class Model:
             self,
             dataset_config: DatasetConfiguration,
             dataloader_config: DataloaderConfiguration,
-    ) -> tuple[DataLoader, dict[str: DataLoader]]:
+    ) -> tuple[DataLoader, dict[str, DataLoader]]:
         dataset_name = dataset_config.get('name', 'CASIA-B')
         if dataset_name == 'CASIA-B':
             gallery_dataset = self._parse_dataset_config(
