b6e5972b64cc61fc967cf3d098fc629d781adce4
Jordan Gong <jordan.gong@protonmail.com>
2021-03-22 19:32:16 +0800

    Add embedding visualization and validate on testing set

--- utils/configuration.py
+++ utils/configuration.py
@@ -14,6 +14,7 @@ class DatasetConfiguration(TypedDict):
     name: str
     root_dir: str
     train_size: int
+    val_size: int
     num_sampled_frames: int
     truncate_threshold: int
     discard_threshold: int
